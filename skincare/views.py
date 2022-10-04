c56ba3b19eb1c780a95d8711f0ce4994903f5794
Faris Dhoot <farisgjd@outlook.com>
2022-10-04 01:01:35 +0000

    Create functionality that renders initial version of charcter indetifiers for brands page

--- skincare/views.py
+++ skincare/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
 from .models import Brand
+
+
+def all_brands(request):
+    """ A view to render all products aswell as sorting and seraching """
+
+    # Renders Brands Model
+    brands = Brand.objects.all()
+
+    # Filters brand starting letter and only renders single ones  
+    brand_letters_query = Brand.objects.values_list('character_identifier')
+    brand_letters = []
+    # for letters in brand_letters_query:
+    #     if letters not in brand_letters:
+    #         brand_letters.append(letters)
+
+    # Allow you to filter by brands use after letters are established
+    brand = brands.filter(character_identifier="a")
+
+    context = {
+        'brands': brands,
+        'brand_letters': brand_letters,
+        'brand': brand,
+    }
+
+    return render(request, 'brands/brands.html', context)
+
+
 # from .models import Skincare
 
 
@@ -13,15 +40,3 @@ from .models import Brand
 #     }
 
 #     return render(request, 'skincare/skincare.html', context)
-
-
-def all_brands(request):
-    """ A view to render all products aswell as sorting and seraching """
-
-    brands = Brand.objects.all()
-    
-    context = {
-        'brands': brands,
-    }
-
-    return render(request, 'brands/brands.html', context)
