ae051b6a2a325c620281431af1df0a4212a91def
Faris Dhoot <farisgjd@outlook.com>
2022-11-16 21:55:57 +0000

    Create basic layout for brand image and brand text on brand details page

--- skincare/urls.py
+++ skincare/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_brands, name='brands'),
+    path('brands/', views.all_brands, name='brands'),
     path('<slug:slug>/', views.full_brands, name='full_brands'),
 ]
