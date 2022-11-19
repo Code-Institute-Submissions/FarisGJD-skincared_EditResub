6a781382ddbaa613e0c6cbebb3c1c67774bc5159
Faris Dhoot <farisgjd@outlook.com>
2022-11-19 22:44:41 +0000

    Gegeneral styling

--- skincare/urls.py
+++ skincare/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('brands/', views.all_brands, name='brands'),
+    path('', views.all_brands, name='brands'),
     path('<slug:slug>/', views.full_brands, name='full_brands'),
 ]
