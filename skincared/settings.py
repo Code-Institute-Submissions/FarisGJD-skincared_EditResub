79ff55d22309961f9f88552d7c025e021274dc45
Faris Dhoot <farisgjd@outlook.com>
2023-03-08 02:22:06 +0000

    Downalod crispy forms and create basic checkout view inclduing form

--- skincared/settings.py
+++ skincared/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     # Other
+    'crispy_forms', 
     'storages',
 ]
 MIDDLEWARE = [
@@ -69,6 +70,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'skincared.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -86,6 +89,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
