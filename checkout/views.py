6c1fe1153181b165962c72c170a91fe4a53fa067
Faris Dhoot <farisgjd@outlook.com>
2023-03-08 21:44:38 +0000

    Create payment intent in view

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Your Bag Is Cuerrently Empty")
@@ -18,6 +21,12 @@ def checkout(request):
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
     stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
