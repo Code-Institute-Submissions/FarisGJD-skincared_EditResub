26eca9eb67fd257663a9fb42dcb49b798c157145
Faris Dhoot <farisgjd@outlook.com>
2023-03-13 19:26:48 +0000

    Add delete functioanlity and links to edit and delte products

--- skincare/views.py
+++ skincare/views.py
@@ -139,21 +139,20 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Product successfully added!')
-            return redirect(reverse('add_product'))
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_details', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
-
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
     }
 
     return render(request, template, context)
-
 
 def edit_product(request, product_id):
     """ Edit a product in the store """
@@ -177,3 +176,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Skincare, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('all_products'))
